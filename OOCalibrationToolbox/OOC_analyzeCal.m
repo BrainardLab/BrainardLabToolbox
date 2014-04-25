93833daf0b747d9a8940e0b9878005fc3b1f2be7
Nicolas Cottaris <cottaris@sas.upenn.edu>
2014-04-25 16:27:32 -0400

    Various updates.

--- OOCalibrationToolbox/OOC_analyzeCal.m
+++ OOCalibrationToolbox/OOC_analyzeCal.m
@@ -8,14 +8,14 @@ function OOC_analyzeCal
     % Load a calibration file
     [calStruct, calFilename] = GetCalibrationStructure('Enter calibration filename','ViewSonicProbe',[]);
     
+    calStruct.describe
+    
     % Analyze the calibration file and display the results arranged in different grids
     calAnalyzer.essentialDataGridDims       = [3 3];
     calAnalyzer.linearityChecksGridDims     = [2 3];  
     calAnalyzer.backgroundEffectsGridDims   = [2 3];  
-    calAnalyzer.analyze(calStruct);
     
-    % Print cal struct
-    calAnalyzer.displayCalStruct();
+    calAnalyzer.analyze(calStruct);
     
     % Exit
     calAnalyzer.shutdown();
