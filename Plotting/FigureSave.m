871cbe1fff5fe2f51ea96e478a9ed7804fc2af03
David Brainard <brainard@psych.upenn.edu>
2014-04-29 08:33:39 -0400

    Added this general calling interface.
    Signed-off-by: David Brainard <brainard@psych.upenn.edu>

---
+++ Plotting/FigureSave.m
@@ -0,0 +1,25 @@
+function FigureSave(figName,figHandle,figType)
+% FigureSave(figName,figHandle,figType)
+%
+% Encapsulates figure saving.  This way you
+% can swap in the method here.
+%
+% Loops over list in figType if figType is a cell array.
+%
+% 4/6/14  dhb  Wrote it
+
+if iscell(figType)
+    for i = 1:length(figType)
+        if (exist('exportfig'))
+            exportfig(figHandle,figName,'FontMode', 'fixed','FontSize', 12,'Width',6,'Height',6, 'color', 'cmyk','Format',figType{i});
+        elseif (exist('savefig'))
+            savefig(figName,figHandle,figType{i});
+        end
+    end
+else
+    if (exist('exportfig'))
+        exportfig(figHandle,figName,'FontMode', 'fixed','FontSize', 12,'Width',6,'Height',6, 'color', 'cmyk','Format',figType);
+    elseif (exist('savefig'))
+        savefig(figName,figHandle,figType);
+    end
+end
