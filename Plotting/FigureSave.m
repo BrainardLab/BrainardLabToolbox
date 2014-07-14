079a7b3f1e2bdc723e1587da8ad2b5d0c99b0434
David Brainard <brainard@psych.upenn.edu>
2014-07-14 14:41:43 -0400

    savefig -> savefigghost in FigureSave.
    Signed-off-by: David Brainard <brainard@psych.upenn.edu>

--- Plotting/FigureSave.m
+++ Plotting/FigureSave.m
@@ -7,12 +7,13 @@ function FigureSave(figName,figHandle,figType)
 % Loops over list in figType if figType is a cell array.
 %
 % 4/6/14  dhb  Wrote it
+% 7/14/14 dhb  savefig -> savefigghost
 
 if iscell(figType)
     for i = 1:length(figType)
         if (exist('exportfig','file'))
             exportfig(figHandle,figName,'FontMode', 'fixed','FontSize', 12,'Width',6,'Height',6, 'color', 'cmyk','Format',figType{i});
-        elseif (ismac & exist('savefig','file'))
+        elseif (ismac & exist('savefigghost','file'))
             savefigghost(figName,figHandle,figType{i});
         else
             saveas(figHangle,figName,figType{i});
@@ -21,8 +22,8 @@ if iscell(figType)
 else
     if (exist('exportfig','file'))
         exportfig(figHandle,figName,'FontMode', 'fixed','FontSize', 12,'Width',6,'Height',6, 'color', 'cmyk','Format',figType);
-    elseif (ismac & exist('savefig','file'))
-        savefig(figName,figHandle,figType);
+    elseif (ismac & exist('savefigghost','file'))
+        savefigghost(figName,figHandle,figType);
     else
         saveas(figHangle,figName,figType{i});
     end
