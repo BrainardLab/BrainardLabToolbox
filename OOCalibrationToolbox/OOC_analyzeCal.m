09e0f55bff2d85a476f9bbecd071b4eba093b5ef
Nicolas Cottaris <cottaris@sas.upenn.edu>
2014-08-13 03:58:12 -0400

    Improvements in visualization @CalibratorAnalyzer

--- OOCalibrationToolbox/OOC_analyzeCal.m
+++ OOCalibrationToolbox/OOC_analyzeCal.m
@@ -9,8 +9,8 @@ function OOC_analyzeCal
     
     % Analyze the calibration file and display the results arranged in different grids
     calAnalyzer.essentialDataGridDims       = [3 3];
-    calAnalyzer.linearityChecksGridDims     = [2 3];  
-    calAnalyzer.backgroundEffectsGridDims   = [2 3];  
+    calAnalyzer.linearityChecksGridDims     = [2 3];  % columns rows
+    calAnalyzer.backgroundEffectsGridDims   = [3 2];  
     
     calAnalyzer.analyze();
 end
