e831ab28688e8c43627bc4d02b88517f1de26f64
np_cottaris <cottaris@sas.upenn.edu>
2014-10-24 15:41:49 -0400

    Small updates

--- PathUtilities/SwitchPathConfig.m
+++ PathUtilities/SwitchPathConfig.m
@@ -7,12 +7,13 @@ function SwitchPathConfig
     fprintf('\n\t[0]. Standard Brainad lab config');
     fprintf('\n\t[1]. Matlab-main only');
     fprintf('\n\t[2]. No select native Matlab toolboxes');
-    fprintf('\n\t[3]. No public BrainardLabToolbox');
-    fprintf('\n\t[4]. No private BrainardLabToolbox');
-    fprintf('\n\t[5]. No public or private BrainardLabToolbox');
-    fprintf('\n\t[6]. No Psychtoolbox');
-    fprintf('\n\t[7]. No public or private BrainardLabToolbox, no Psychtoolbox');
-    fprintf('\n\t[8]. No ISETBIO');
+    fprintf('\n\t[3]. No native Matlab toolboxes whatsoever');
+    fprintf('\n\t[4]. No public BrainardLabToolbox');
+    fprintf('\n\t[5]. No private BrainardLabToolbox');
+    fprintf('\n\t[6]. No public or private BrainardLabToolbox');
+    fprintf('\n\t[7]. No Psychtoolbox');
+    fprintf('\n\t[8]. No public or private BrainardLabToolbox, no Psychtoolbox');
+    fprintf('\n\t[9]. No ISETBIO');
     
     pathSelection = input('\nSelect a configuration [default = 0]: ');
     if (isempty(pathSelection))
@@ -33,24 +34,27 @@ function SwitchPathConfig
             PathConfig.removeNativeToolboxes(nativeToolboxesDirList);
             
         case 3
+            PathConfig.removeNativeToolboxes({});
+            
+        case 4
             PathConfig.removeNonNativeToolboxes('/Users/Shared/Matlab/Toolboxes/BrainardLabToolbox');
           
-        case 4
-            PathConfig.removeNonNativeToolboxes('/Users/Shared/Matlab/Toolboxes/BrainardLabPrivateToolbox');
-        
         case 5
-            PathConfig.removeNonNativeToolboxes('/Users/Shared/Matlab/Toolboxes/BrainardLabToolbox');
             PathConfig.removeNonNativeToolboxes('/Users/Shared/Matlab/Toolboxes/BrainardLabPrivateToolbox');
         
         case 6
+            PathConfig.removeNonNativeToolboxes('/Users/Shared/Matlab/Toolboxes/BrainardLabToolbox');
+            PathConfig.removeNonNativeToolboxes('/Users/Shared/Matlab/Toolboxes/BrainardLabPrivateToolbox');
+        
+        case 7
             PathConfig.removeNonNativeToolboxes('/Users/Shared/Matlab/Toolboxes/Psychtoolbox-3');
             
-        case 7
+        case 8
             PathConfig.removeNonNativeToolboxes('/Users/Shared/Matlab/Toolboxes/BrainardLabToolbox');
             PathConfig.removeNonNativeToolboxes('/Users/Shared/Matlab/Toolboxes/BrainardLabPrivateToolbox');
             PathConfig.removeNonNativeToolboxes('/Users/Shared/Matlab/Toolboxes/Psychtoolbox-3');
             
-        case 8
+        case 9
             PathConfig.removeNonNativeToolboxes('/Users/Shared/Matlab/Toolboxes/isetbio');
             
         otherwise
