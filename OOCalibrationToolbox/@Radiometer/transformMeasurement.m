c2f27c459b83ecaa5247000cc75c316755135965
np_cottaris <cottaris@sas.upenn.edu>
2015-03-11 11:53:47 -0400

    Code restructuring
    Signed-off-by: np_cottaris <cottaris@sas.upenn.edu>

--- OOCalibrationToolbox/@Radiometer/transformMeasurement.m
+++ OOCalibrationToolbox/@Radiometer/transformMeasurement.m
@@ -1,30 +1,37 @@
 % Method to transform a native measurement according to user-supplied S and T matrices
 function measurement = transformMeasurement(obj, applyUserS, applyUserT)
+    
     if (isempty(obj.nativeMeasurement.energy))
         fprintf('There is no measurement yet !\n');
         measurement = [];
-    else
-        % resample SPD according to passed userS
-        resampledSPD = [];
-        if (applyUserS)
-            resampledSPD = SplineSpd(obj.nativeS, obj.nativeMeasurement.energy', obj.userS);
-
-            % update measurement
-            measurement = struct( ...
-                'spectralAxis', linspace(obj.userS(1),obj.userS(1)+(obj.userS(3)-1)*obj.userS(2),obj.userS(3)), ...
-                'energy',       resampledSPD ...
-                );
-        end
-
-        % transform SPD according to passed userT
-        if (applyUserT)
-            if (~isempty(resampledSPD))
-                effectiveSPD = resampledSPD;
-            else
-                effectiveSPD = obj.nativeMeasurement.energy';
-            end
-            result = obj.userT * effectiveSPD;
-            measurement = result;
-        end
+        return;
     end
+    
+    resampledSPD = [];
+    
+    % applyUserS
+    if (applyUserS)
+        % resample SPD according to passed userS
+        resampledSPD = SplineSpd(obj.nativeS, obj.nativeMeasurement.energy', obj.userS);
+
+        % update measurement
+        measurement = struct( ...
+            'spectralAxis', SToWls(obj.userS), ...
+            'energy',       resampledSPD ...
+            );
+    end
+
+    % applyUserS
+    if (applyUserT)    
+        if (~isempty(resampledSPD))
+            effectiveSPD = resampledSPD;
+        else
+            effectiveSPD = obj.nativeMeasurement.energy';
+        end
+        
+        % transform SPD according to passed userT
+        result = obj.userT * effectiveSPD;
+        measurement = result;
+    end
+    
 end % obj = transformMeasurement(obj)
