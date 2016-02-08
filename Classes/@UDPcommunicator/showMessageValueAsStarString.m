4af1a0b92a74741ed1a93c3fa744567a129127db
Nicolas Cottaris <cottaris@sas.upenn.edu>
2016-02-08 14:43:25 -0500

    Update
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- Classes/@UDPcommunicator/showMessageValueAsStarString.m
+++ Classes/@UDPcommunicator/showMessageValueAsStarString.m
@@ -2,13 +2,13 @@ function showMessageValueAsStarString(obj, direction, msgLabel, msgValueType, ms
 
     if (strcmp(lower(msgValueType), 'string'))
         starsNum = length(msgValue)/maxValue*maxStars;  % length of string
-    elseif (strcmp(lower(msgValueType, 'boolean')))
+    elseif (strcmp(lower(msgValueType), 'boolean'))
         if (msgValue)
             starsNum = maxStars-1
         else
             starsNum = 1;
         end
-    elseif (strcmp(lower(msgValueType, 'numeric')))
+    elseif (strcmp(lower(msgValueType), 'numeric'))
         starsNum = msgValue / maxValue * maxStars;
     end
     
@@ -20,13 +20,13 @@ function showMessageValueAsStarString(obj, direction, msgLabel, msgValueType, ms
     
     if (strcmp(lower(msgValueType), 'string'))
         fprintf('\n %10s %-20s (%10s %s): %40s', direction, msgLabel, msgValueType, msgValue, msg);
-    elseif (strcmp(lower(msgValueType, 'boolean')))
+    elseif (strcmp(lower(msgValueType), 'boolean'))
         if (msgValue)
             fprintf('\n %10s %-20s (%10s TRUE ): %40s', direction, msgLabel, msgValueType, msg);
         else
             fprintf('\n %10s %-20s (%10s FALSE): %40s', direction, msgLabel, msgValueType, msg);
         end
-    elseif (strcmp(lower(msgValueType, 'numeric')))
+    elseif (strcmp(lower(msgValueType), 'numeric'))
         fprintf('\n %10s %-20s (%10s %3d): %40s', direction, msgLabel, msgValueType, msgValue, msg);
     end
     
