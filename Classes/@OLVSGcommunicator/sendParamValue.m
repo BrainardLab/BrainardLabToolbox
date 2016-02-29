019fa6252fb3da16d92fc71761402916750f4a50
Nicolas Cottaris <cottaris@sas.upenn.edu>
2016-02-29 16:35:34 -0500

    Updates
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- Classes/@OLVSGcommunicator/sendParamValue.m
+++ Classes/@OLVSGcommunicator/sendParamValue.m
@@ -9,20 +9,21 @@ function sendParamValue(obj, paramName, paramValue, varargin)
     defaultTimeOutSecs = 2;
     defaultMaxAttemptsNum = 3;
     p = inputParser;
+    p.addRequired('obj');
     p.addRequired('paramName', @ischar);
     p.addRequired('paramValue');
     p.addParamValue('timeOutSecs', defaultTimeOutSecs,   @isnumeric);
     p.addParamValue('maxAttemptsNum', defaultMaxAttemptsNum, @isnumeric);
-    p.parse(varargin{:});
+    p.parse(obj, paramName, paramValue, varargin{:});
     
     
     % Send the message
     messageValue = p.Results.paramValue;
     messageLabel = p.Results.paramName;
     if (isempty(messageValue))
-        status = obj.sendMessage(messageLabel, p.Results.timeOutSecs, 'maxAttemptsNum', p.Results.maxAttemptsNum);
+        status = p.Results.obj.sendMessage(messageLabel, p.Results.timeOutSecs, 'maxAttemptsNum', p.Results.maxAttemptsNum);
     else
-        status = obj.sendMessage(messageLabel, 'withValue', messageValue, 'timeOutSecs', p.Results.timeOutSecs, 'maxAttemptsNum', p.Results.maxAttemptsNum);
+        status = p.Results.obj.sendMessage(messageLabel, 'withValue', messageValue, 'timeOutSecs', p.Results.timeOutSecs, 'maxAttemptsNum', p.Results.maxAttemptsNum);
     end
     
     % Get this backtrace of all functions leading to this point
@@ -34,5 +35,5 @@ function sendParamValue(obj, paramName, paramValue, varargin)
     end
     
     % Check status to ensure we received a 'TRANSMITTED_MESSAGE_MATCHES_EXPECTED' message
-    assert(strcmp(status, obj.TRANSMITTED_MESSAGE_MATCHES_EXPECTED), sprintf('%s: Exiting due to mismatch in message labels.\nExpected label: ''%s'', Received label: ''%s''.\n', backTrace, obj.TRANSMITTED_MESSAGE_MATCHES_EXPECTED, status));
+    assert(strcmp(status, p.Results.obj.TRANSMITTED_MESSAGE_MATCHES_EXPECTED), sprintf('%s: Exiting due to mismatch in message labels.\nExpected label: ''%s'', Received label: ''%s''.\n', backTrace, p.Results.obj.TRANSMITTED_MESSAGE_MATCHES_EXPECTED, status));
 end
