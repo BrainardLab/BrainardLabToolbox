2b851fb29028d8d729eca982ca325338a0a5acf8
Nicolas Cottaris <cottaris@sas.upenn.edu>
2016-03-01 09:20:47 -0500

    bug fix
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- Classes/@OLVSGcommunicator/validateValueForParam.m
+++ Classes/@OLVSGcommunicator/validateValueForParam.m
@@ -1,8 +1,8 @@
 function validateValueForParam(obj, paramName, paramValue, backTrace)
-    if (isKey(obj.validValues, paramName))
+    if (isKey(obj.validParamValues, paramName))
         % We have valid range for paramName
         % check if we are within it
-        if (~ismember(paramValue, obj.validValues(paramName)))
+        if (~ismember(paramValue, obj.validParamValues(paramName)))
             error('%s: Received invalid value (''%s'') for param ''%s''.', backTrace, paramValue, paramName);
         end
     end
