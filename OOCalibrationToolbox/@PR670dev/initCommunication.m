e88026776f5e093a644a46b400f4eaacff679759
melanopsin <melanopsin@melanopsins-Mac-Pro.local>
2016-08-15 12:06:01 -0400

    Added function to check PR670 status

--- OOCalibrationToolbox/@PR670dev/initCommunication.m
+++ OOCalibrationToolbox/@PR670dev/initCommunication.m
@@ -22,15 +22,15 @@ function obj = initCommunication(obj)
                                    'appendCR', false);
 
         pause(3.0);
-        fprintf('\n Waiting for PR670 to acknowledge remote mode for up to 10 seconds ...');
+        fprintf('\nWaiting for PR670 to acknowledge remote mode for up to 10 seconds ...\n');
         % Get the response.  Timeout after 10 seconds.  
         timeoutInSeconds = 10;
-        response = obj.getResponseOrTimeOut(timeoutInSeconds, 'Failed to set PR670 in REMOTE  MODE. Disconnect PR670, recycle its power, and try again');
+        response = obj.getResponseOrTimeOut(timeoutInSeconds, '\nFailed to set PR670 in REMOTE  MODE. Disconnect PR670, recycle its power, and try again\n');
         
         if (isempty(response ))
             error('Count not read response from PR670');
         elseif strcmp(response , ' REMOTE MODE')
-           fprintf('Sucessfully established communication with PR670!!\n\n'); 
+           fprintf('<strong>Sucessfully established communication with PR670!!</strong>\n\n'); 
         else
             error('*** Wrong response from PR670. \n\tExpected: '' REMOTE MODE''\n\tReceived: ''%s''\n.', response );
         end
---
+++ OOCalibrationToolbox/OOC_checkPR670.m
@@ -0,0 +1,32 @@
+function OOC_checkPR670(varargin)
+
+    if (nargin == 0)
+        verb = 0;
+    else
+        verb = varargin{1};
+        if (isnumeric(verb) && verb >= 0 && verb <= 10)
+        else
+            verb = 0;
+        end
+    end
+
+    pr670obj = [];
+    devicePortString = '';  % or select from ls -a /dev/cu*, e.g. '/dev/cu.usbmodem1a21'
+    
+    try
+        pr670obj = PR670dev('verbosity', verb, 'devicePortString', devicePortString);
+        fprintf('<strong>Hit enter to close device: </strong>');
+        pause
+        fprintf('Please wait ...');
+        pr670obj.shutDown();
+        
+    catch err
+        fprintf('<strong>\nEncountered the following error: %s</strong>\n', err.message);
+        if (~isempty(pr670obj))
+            pr670obj.shutDown();
+        end
+        rethrow(err);
+    end
+    
+end
+
