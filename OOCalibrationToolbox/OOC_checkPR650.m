bb59143d6c29e2bfb15aed109e2e3b33a761147e
Nicolas Cottaris <cottaris@sas.upenn.edu>
2016-08-16 18:16:43 -0400

    Added OCC_checkPR650
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

---
+++ OOCalibrationToolbox/OOC_checkPR650.m
@@ -0,0 +1,32 @@
+function OOC_checkPR650(varargin)
+
+    if (nargin == 0)
+        verb = 0;
+    else
+        verb = varargin{1};
+        if (isnumeric(verb) && verb >= 0 && verb <= 10)
+        else
+            verb = 0;
+        end
+    end
+
+    pr650obj = [];
+    devicePortString = '';  % or select from ls -a /dev/cu*, e.g. '/dev/cu.usbmodem1a21'
+    
+    try
+        pr650obj = PR650dev('verbosity', verb, 'devicePortString', devicePortString);
+        fprintf('<strong>Hit enter to close device: </strong>');
+        pause
+        fprintf('Please wait ...');
+        pr650obj.shutDown();
+        
+    catch err
+        fprintf('<strong>\nEncountered the following error: %s</strong>\n', err.message);
+        if (~isempty(pr650obj))
+            pr650obj.shutDown();
+        end
+        rethrow(err);
+    end
+    
+end
+
