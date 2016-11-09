d0d4ad8dc56755ea1399d317bf0a89221b002dd9
David Brainard <brainard@psych.upenn.edu>
2016-11-09 15:31:07 -0500

    Add optional argument
    Signed-off-by: David Brainard <brainard@psych.upenn.edu>

--- ExperimentSupport/@Staircase/getTrials.m
+++ ExperimentSupport/@Staircase/getTrials.m
@@ -1,17 +1,28 @@
-function [values,responses] = getTrials(obj)
-% [values,responses] = getTrials(obj)
+function [values,responses] = getTrials(obj,nTrialsDiscard)
+% [values,responses] = getTrials(obj,[nTrialsDiscard])
 %
 % Description: Returns the values and responses for
 %   trials run so far.
 %
 % Required Input:
 %   obj - Staircase object.
+%   nTrialsDiscard (default 0)
+%
+% If nTrialsDiscard is set to an integer, the first nTrialsDiscard of the
+% staircase are not returned.
 %
 % Output:
 %   values - Vector of values
 %   responses - Vector of responses
 %
+% Empty matrices are returned if there were not enough trials.
+%
 % 10/19/09  dhb  Wrote it.
+% 09/1916   dhb  Add nTrialsDiscard argument.
+
+if (nargin < 2 | isempty(nTrialsDiscard))
+    nTrialsDiscard = 0;
+end
 
 switch obj.StaircaseType
 	case 'standard'
@@ -19,10 +30,10 @@ switch obj.StaircaseType
 	case 'quest'
 end
 
-if (obj.NextTrial == 1)
+if (obj.NextTrial-1 <= nTrialsDiscard)
     values = [];
     responses = [];
 else
-    values = obj.Values(1:obj.NextTrial-1);
-    responses = obj.Responses(1:obj.NextTrial-1);
+    values = obj.Values((nTrialsDiscard+1):obj.NextTrial-1);
+    responses = obj.Responses((nTrialsDiscard+1):obj.NextTrial-1);
 end
