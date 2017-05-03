06219bca8a909fc3c5bd17a6012a88395758bf7b
David Brainard <brainard@psych.upenn.edu>
2017-05-03 15:01:06 -0400

    Put in even fancier start stuff
    Signed-off-by: David Brainard <brainard@psych.upenn.edu>

--- ColorMaterialModel/ColorMaterialModelCrossValidateRealData.m
+++ ColorMaterialModel/ColorMaterialModelCrossValidateRealData.m
@@ -1,13 +1,13 @@
 % ColorMaterialModelCrossValidationRealData
-% Perform cross valiadation on uses it to extract model paramters. 
+% Perform cross valiadation on uses it to extract model paramters.
 %
 % 03/17/2017 ar Wrote it.
-% 04/30/2017 ar Clean up. Adding comments. 
+% 04/30/2017 ar Clean up. Adding comments.
 
 % Initialize
 clear; close all;
 
-% Load the look up table. Set experiment to analyze. 
+% Load the look up table. Set experiment to analyze.
 load colorMaterialInterpolateFunCubiceuclidean.mat
 whichExperiment = 'Pilot';
 
@@ -36,14 +36,14 @@ nConditions = length(conditionCode);
 params.F = colorMaterialInterpolatorFunction;
 params.trySpacingValues = [0.5 1 2 3];
 params.tryWeightValues = [0.5 0.2 0.8];
-           
+
 params.maxPositionValue = 20;
 params.whichMethod = 'lookup';
 params.nSimulate = 1000;
 whichWeight = 'weightVary';
 
 % Set cross validation parameters
-nModelTypes = 1; 
+nModelTypes = 1;
 for s = 1:nSubjects
     switch whichExperiment
         case 'Pilot'
@@ -54,53 +54,70 @@ for s = 1:nSubjects
             load([figAndDataDir '/' subjectList{s} 'SummarizedDataFULL.mat']); % data
     end
     
-    for whichModelType = 1:nModelTypes
-        if whichModelType == 1
-            params.whichWeight = whichWeight;
-            params.whichPositions = 'full';
-            thisSubject.model{whichModelType} = 'Full-Vary';
-        elseif whichModelType == 2
-            params.whichWeight = 'weightVary';
-            params.whichPositions = 'smoothSpacing';
-            params.smoothOrder = 1;
-            thisSubject.model{whichModelType}  = 'SmoothLinear-Vary';
-        elseif whichModelType == 3
-            params.whichWeight = 'weightVary';
-            params.whichPositions = 'smoothSpacing';
-            params.smoothOrder = 3;
-            thisSubject.model{whichModelType}  = 'SmoothCubic-Vary';
-        end
-        
-        % Partition data for cross validation.
-        c = cvpartition(nBlocks,'Kfold',nFolds);
-                
-        for whichCondition = 1:nConditions
-            for kk = 1:c.NumTestSets
-                
-                clear trainingIndex testIndex trainingData testData nTrainingTrials nTestTrials probabilitiesTestData
-                
-                % Get indices for kkth fold
-                trainingIndex = c.training(kk);
-                testIndex = c.test(kk);
-                
-                % Separate the training from test data
-                trainingData = sum(thisSubject.condition{whichCondition}.firstChosenPerTrial(:,trainingIndex),2);
-                testData = sum(thisSubject.condition{whichCondition}.firstChosenPerTrial(:,testIndex),2);
-                nTrainingTrials = c.TrainSize(kk)*ones(size(trainingData));
-                nTestTrials = c.TestSize(kk)*ones(size(testData));
-                probabilitiesTestData = testData./nTestTrials;
+    % Partition data for cross validation.
+    c = cvpartition(nBlocks,'Kfold',nFolds);
+    
+    for whichCondition = 1:nConditions
+        for kk = 1:c.NumTestSets
+            
+            clear trainingIndex testIndex trainingData testData nTrainingTrials nTestTrials probabilitiesTestData
+            
+            % Get indices for kkth fold
+            trainingIndex = c.training(kk);
+            testIndex = c.test(kk);
+            
+            % Separate the training from test data
+            trainingData = sum(thisSubject.condition{whichCondition}.firstChosenPerTrial(:,trainingIndex),2);
+            testData = sum(thisSubject.condition{whichCondition}.firstChosenPerTrial(:,testIndex),2);
+            nTrainingTrials = c.TrainSize(kk)*ones(size(trainingData));
+            nTestTrials = c.TestSize(kk)*ones(size(testData));
+            probabilitiesTestData = testData./nTestTrials;
+            
+            for whichModelType = 1:nModelTypes
+                if whichModelType == 1
+                    params.whichWeight = whichWeight;
+                    params.whichPositions = 'smoothSpacing';
+                    params.smoothOrder = 1;
+                    tryColorSpacingValues = params.trySpacingValues;
+                    tryMaterialSpacingValues = params.trySpacingValues;
+                    tryWeightValues = params.tryWeightValues;
+                elseif whichModelType == 2
+                    params.whichWeight = whichWeight;
+                    params.whichPositions = 'smoothSpacing';
+                    params.smoothOrder = 3;
+                    tryColorSpacingValues = [params.trySpacingValues linearSolutionColorSlope];
+                    tryMaterialSpacingValues = [params.trySpacingValues linearSolutionMaterialSlope];
+                    tryWeightValues = [params.tryWeightValues linearSolutionWeight]; 
+                elseif whichModelType == 3
+                    params.whichWeight = whichWeight;
+                    params.whichPositions = 'full';
+                    tryColorSpacingValues = [params.trySpacingValues linearSolutionColorSlope];
+                    tryMaterialSpacingValues = [params.trySpacingValues linearSolutionMaterialSlope];
+                    tryWeightValues = [params.tryWeightValues linearSolutionWeight]; 
+                end
                 
                 % Get the predictions from the model for current parameters
                 [thisSubject.condition{whichCondition}.crossVal(whichModelType).returnedParamsTraining(kk,:), ...
                     thisSubject.condition{whichCondition}.crossVal(whichModelType).logLikelyFitTraining(kk), ...
-                    thisSubject.condition{whichCondition}.crossVal(whichModelType).predictedProbabilitiesBasedOnSolutionTraining(kk,:), ...
-                    thisSubject.condition{whichCondition}.crossVal(whichModelType).kTraining(kk)] = ...
+                    thisSubject.condition{whichCondition}.crossVal(whichModelType).predictedProbabilitiesBasedOnSolutionTraining(kk,:)] = ...
                     FitColorMaterialModelMLDS(pairColorMatchColorCoords, pairMaterialMatchColorCoords,...
                     pairColorMatchMaterialCoords, pairMaterialMatchMaterialCoords,...
                     trainingData, nTrainingTrials,params, ...
                     'whichPositions',params.whichPositions,'whichWeight',params.whichWeight, ...
-                    'tryWeightValues',params.tryWeightValues,'trySpacingValues',params.trySpacingValues, ...
+                    'tryWeightValues',params.tryWeightValues,'tryColorSpacingValues',params.tryColorSpacingValues,'tryMaterualSpacingValues',params.tryMaterialSpacingValues, ...
                     'maxPositionValue', params.maxPositionValue);
+                
+                % For linear model, grab solution to use as a start when we search for
+                % the cubic and full solutions.
+                %
+                % ANA TO CHECK WHICH IS MATERIAL SLOPE AND WHICH ONE IS THE
+                % COLOR SLOPE.
+                if (whichModelType == 1)
+                    linearSolutionParameters = thisSubject.condition{whichCondition}.crossVal(whichModelType).returnedParamsTraining(kk,:);
+                    linearSolutionColorSlope = linearSolutionParameters(1);
+                    linearSolutionMaterialSlope = linearSolutionParameters(2);
+                    linearSolutionWeight = linearSolutionParameters(3);
+                end
                 
                 % Now use these parameters to predict the responses for the test data.
                 [negLogLikely,predictedResponses] = FitColorMaterialModelMLDSFun(thisSubject.condition{whichCondition}.crossVal(whichModelType).returnedParamsTraining(kk,:),...
@@ -116,16 +133,16 @@ for s = 1:nSubjects
                     ComputeRealRMSE(predictedResponses, probabilitiesTestData);
                 clear negLogLikely predictedResponses
             end
-            
-            % Compute mean error for both log likelihood and rmse.
-            thisSubject.condition{whichCondition}.crossVal(whichModelType).meanRMSError = ...
-                mean(thisSubject.condition{whichCondition}.crossVal(whichModelType).RMSError);
-            thisSubject.condition{whichCondition}.crossVal(whichModelType).meanLogLikelihood = ...
-                mean(thisSubject.condition{whichCondition}.crossVal(whichModelType).LogLikelyhood);
-            
-            % Save in the right folder.
-            cd(figAndDataDir);
-            save([subjectList{s} conditionCode{whichCondition} params.whichWeight '-' num2str(nFolds) 'Folds'],  'thisSubject');
-        end
+        end 
+        
+        % Compute mean error for both log likelihood and rmse.
+        thisSubject.condition{whichCondition}.crossVal(whichModelType).meanRMSError = ...
+            mean(thisSubject.condition{whichCondition}.crossVal(whichModelType).RMSError);
+        thisSubject.condition{whichCondition}.crossVal(whichModelType).meanLogLikelihood = ...
+            mean(thisSubject.condition{whichCondition}.crossVal(whichModelType).LogLikelyhood);
+        
+        % Save in the right folder.
+        cd(figAndDataDir);
+        save([subjectList{s} conditionCode{whichCondition} params.whichWeight '-' num2str(nFolds) 'Folds'],  'thisSubject');
     end
 end
