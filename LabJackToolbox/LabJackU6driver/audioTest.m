4357a9dafb1e8ab7716f4f38c2a8c8149220616e
Nicolas Cottaris <cottaris@upenn.edu>
2017-06-06 11:49:20 -0400

    Audio files added

--- LabJackToolbox/LabJackU6driver/audioTest.m
+++ LabJackToolbox/LabJackU6driver/audioTest.m
@@ -1,11 +1,17 @@
 function audioTest
 
-    frequencyHz = 100;
+    frequencyHz = 30;
     samplingFrequencyHz = 50000;
     dt = 1/samplingFrequencyHz;
     duration = 5.0;
     t = 0:dt:duration;
-    y = 1/2*sin(2*pi*frequencyHz*t);
-    audiowrite('100Hz.wav',y, samplingFrequencyHz);
+    tt = t-mean(t);
+    sigma = mean(t)/6;
+    envelope = (exp(-0.5*(tt/sigma).^2)).^0.25;
+    y = 1/2*sin(2*pi*frequencyHz*tt) .* envelope;
+    figure(1); clf;
+    plot(t,y);
+    drawnow;
+    audiowrite('30Hz.wav',y, samplingFrequencyHz);
 end
 
