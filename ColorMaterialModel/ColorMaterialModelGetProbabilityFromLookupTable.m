212d9c910c109d1d02d8cec07859d51664b225d5
aradonjic <radonjic@sas.upenn.edu>
2017-06-19 17:43:18 -0400

    deleted example structure; added a few comments.

--- ColorMaterialModel/ColorMaterialModelGetProbabilityFromLookupTable.m
+++ ColorMaterialModel/ColorMaterialModelGetProbabilityFromLookupTable.m
@@ -3,15 +3,16 @@
 function probability = ColorMaterialModelGetProbabilityFromLookupTable(F,colorMatchColorCoordGrid,materialMatchColorCoordGrid,...
     colorMatchMaterialCoordGrid,materialMatchMaterialCoordsGrid, weightGrid)
 
-% Read probabilities from the lookup table
+% Read probabilities from the lookup table. 
 % Input: 
+%   F - gridded interpolation function based on a look up table. 
 % 	colorMatchColorCoordGrid - current position of color match on color dimension
 %   materialMatchColorCoordGrid - current position of material match on color dimension
 %   colorMatchMaterialCoordGrid - current position of color match on material dimension
 %   materialMatchMaterialCoordsGrid - current position of the material match on material dimension
 %   weightGrid - current weight
 % Output: 
-%   probability - 
+%   probability - for passed paramters, based on the interpolated lookup table.   
 
 % 2/2/17 ar Wrote it. 
 
