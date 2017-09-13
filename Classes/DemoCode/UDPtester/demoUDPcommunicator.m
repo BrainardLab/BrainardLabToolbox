8eae7ff32d1e872200793adbb8070ee3d5f3544a
Nicolas Cottaris <cottaris@sas.upenn.edu>
2017-09-13 15:56:44 -0400

    Started working on demoUDPcommunicator.
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

---
+++ Classes/DemoCode/UDPtester/demoUDPcommunicator.m
@@ -0,0 +1,51 @@
+function demoUDPcommunicator
+
+    % Get computer info
+    systemInfo = GetComputerInfo();
+    
+    %% Instantiate a UDPcommunicator object according to computer name
+    % In this demo we have IPs for 2 computers: manta.psych.upenn.edu and ionean.psych.upenn.edu
+    if (strfind(systemInfo.networkName, 'manta'))
+        UDPobj = UDPcommunicator( ...
+            'localIP',   '128.91.12.90', ...    % REQUIRED: the IP of manta.psych.upenn.edu (local host)
+            'remoteIP',  '128.91.12.144', ...   % REQUIRED: the IP of ionean.psych.upenn.edu (remote host)
+            'verbosity', 'min', ...             % OPTIONAL, with default value: 'normal', and possible values: {'min', 'normal', 'max'},
+            'useNativeUDP', false ...           % OPTIONAL, with default value: false (i.e., using the brainard lab matlabUDP mexfile)
+        );
+    elseif (strfind(systemInfo.networkName, 'ionean'))
+        UDPobj = UDPcommunicator( ...
+        'localIP',   '128.91.12.144', ...       % REQUIRED: the IP of ionean.psych.upenn.edu (local host)
+        'remoteIP',  '128.91.12.90', ...        % REQUIRED: the IP of manta.psych.upenn.edu (remote host)
+        'verbosity', 'min', ...                 % OPTIONAL, with default value: 'normal', and possible values: {'min', 'normal', 'max'},
+        'useNativeUDP', false ...               % OPTIONAL, with default value: false (i.e., using the brainard lab matlabUDP mexfile)
+        );
+    else
+        error('No configuration for computer named ''s''.', systemInfo.networkName);
+    end
+
+    %% We begin by setting up 'manta' as a slave (listener) and 'ionean' as master (emitter)
+    % Configure the message we are expecting and how long we should wait for it
+    syncMessage = 'GO !';
+    if (strfind(systemInfo.networkName, 'manta'))
+        % Wait for ever to receive the syncMessage
+        receiverTimeOutSecs = Inf;   
+        % Start listening
+        messageReceived = UDPobj.waitForMessage(syncMessage, ...
+            'timeOutSecs', receiverTimeOutSecs...
+            );
+        % Feedback to user
+        fprintf('''%s'' received: ''%s''\n', systemInfo.networkName, messageReceived);
+    else
+        % Wait for 4 secs to receive ack that the syncMessage was received
+        acknowledgmentTimeOutSecs = 4;   
+        % Send the SYNC message
+        ackReceived = UDPobj.sendMessage(syncMessage, ...
+            'timeOutSecs', acknowledgmentTimeOutSecs, ...
+            'maxAttemptsNum', 1 ...
+        );
+        % Feedback to user
+        fprintf('''%s'' sent ''%s'' and received the following acknowledgment: ''%s''\n', systemInfo.networkName, syncMessage, ackReceived);
+    end
+
+end
+
