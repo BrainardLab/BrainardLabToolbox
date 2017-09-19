ef848e1cf447a0eee46b0ca830faa8e99145791a
Nicolas Cottaris <cottaris@sas.upenn.edu>
2017-09-19 10:36:39 -0400

    Updates
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- Classes/@UDPcommunicator2/displayMessage.m
+++ Classes/@UDPcommunicator2/displayMessage.m
@@ -1,17 +1,17 @@
 function displayMessage(obj, hostName, action,  messageLabel, messageData, packetNo)
 
     if (isempty(messageData))
-        fprintf('\n<strong>%s</strong> %s \n\tpacket %04d with message label ''%s'' and no attached data].\n', hostName, action, packetNo, messageLabel);
+        fprintf('\n<strong>%s</strong> %s \n\tpacket %04d : message label = ''%s'' ; message data: none \n', hostName, action, packetNo, messageLabel);
     elseif (isstruct(messageData))
-        fprintf('\n<strong>%s</strong> %s \n\tpacket %04d with message label ''%s'' and the following struct data].\n', hostName, action, packetNo, messageLabel);
+        fprintf('\n<strong>%s</strong> %s \n\tpacket %04d : message label = ''%s'' ; message data: struct\n', hostName, action, packetNo, messageLabel);
         messageData
         fprintf('\n');
     elseif (isnumeric(messageData))
-        fprintf('\n<strong>%s</strong> %s \n\tpacket %04d with message label ''%s'' and the following numeric data: %g].\n', hostName, action, packetNo, messageLabel, messageData);
+        fprintf('\n<strong>%s</strong> %s \n\tpacket %04d : message label = ''%s'' ; message data: %g\n', hostName, action, packetNo, messageLabel, messageData);
     elseif (ischar(messageData))
-        fprintf('\n<strong>%s</strong> %s \n\tpacket %04d with message label ''%s'' and the following char data: %s]\n', hostName, action, packetNo, messageLabel, messageData);
+        fprintf('\n<strong>%s</strong> %s \n\tpacket %04d : message label = ''%s'' ; message data: %s\n', hostName, action, packetNo, messageLabel, messageData);
     else
-        fprintf('\n<strong>%s</strong> %s \n\tpacket %04d with message label ''%s'' and the following data of type %s].\n', hostName, action, packetNo, messageLabel, class(messageData));
+        fprintf('\n<strong>%s</strong> %s \n\tpacket %04d : message label = ''%s'' ; message data: \n', hostName, action, packetNo, messageLabel, class(messageData));
         messageData
         fprintf('\n');
     end
