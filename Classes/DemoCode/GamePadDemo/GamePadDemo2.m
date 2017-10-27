76dfaa22152dc10b0a6509b74251f2895a4de50f
Nicolas Cottaris <cottaris@sas.upenn.edu>
2017-10-27 14:33:16 -0400

    Comments
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- Classes/DemoCode/GamePadDemo/GamePadDemo2.m
+++ Classes/DemoCode/GamePadDemo/GamePadDemo2.m
@@ -1,3 +1,14 @@
+% Script to demo the use of the @GamePad class, which allows very fast 
+% access to the Logiteck gamepad.
+%
+% Caution: When using a USB hub to connect the Logitech gamepad (and/or) other components
+% unpredicted behavior may occur. This would need to be troubleshooted by
+% unplugging, one a time, the various components from the hub, or by connecting
+% all components directly to the computer.
+%
+% 10/27/2017 npc Wrote it.
+%
+
 function GamePadDemo2
 
     % Instantiate a gamePad object
