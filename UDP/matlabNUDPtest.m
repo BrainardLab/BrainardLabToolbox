137adc47fcab1da452e33c1e5a48d338980ad0bb
Nicolas Cottaris <cottaris@sas.upenn.edu>
2017-10-27 12:51:57 -0400

    Updates to test
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- UDP/matlabNUDPtest.m
+++ UDP/matlabNUDPtest.m
@@ -4,22 +4,41 @@ function matlabNUDPtest
     
     sattelite1.IP = '128.91.12.144'; % ionean
     sattelite1.portID = 2007;
-    satellite1.ID = 2;
+    sattelite1.ID = 2;
+    
+    sattelite2.IP = '128.91.12.155'; % leviathan
+    sattelite2.portID = 2008;
+    sattelite2.ID = 4;
     
     
-    matlabNUDP('close', satellite1.ID);
-    matlabNUDP('open', baseIP, satellite1.ID, sattelite1.IP, sattelite1.portID);
-
+    % Close sattelites
+    matlabNUDP('close', sattelite1.ID);
+    matlabNUDP('close', sattelite1.ID);
+    
+    % Open connections to 2 sattelites
+    matlabNUDP('open', baseIP, sattelite1.ID, sattelite1.IP, sattelite1.portID);
+    matlabNUDP('open', baseIP, sattelite2.ID, sattelite2.IP, sattelite2.portID);
+    
     disp('Hit enter to send a message\n');
     pause
-    message = 'Hello from manta';
+    message = 'Hello from manta to Ionean';
     matlabNUDP('send', satellite1.ID, message);
+    
+    message = 'Hello from manta to Leviathan';
+    matlabNUDP('send', satellite2.ID, message);
     
     disp('Hit enter to read a message\n');
     pause
     while (matlabNUDP('check', satellite1.ID) == 0)
     end
-    receivedMessage = matlabNUDP('receive', satellite1.ID)
+    receivedMessageFromIonean = matlabNUDP('receive', satellite1.ID)
+    
+    pause
+    while (matlabNUDP('check', satellite2.ID) == 0)
+    end
+    receivedMessageFromLeviathan = matlabNUDP('receive', satellite2.ID)
+    
     matlabNUDP('close', satellite1.ID);
+    matlabNUDP('close', satellite2.ID);
 end
 
