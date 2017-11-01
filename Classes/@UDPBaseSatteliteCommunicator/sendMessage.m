bb4e839756e6fe69d66e4cf1638fd78e778404d5
npcottaris <cottaris@sas.upenn.edu>
2017-11-01 13:15:36 -0400

    remove debug fprintfs

--- Classes/@UDPBaseSatteliteCommunicator/sendMessage.m
+++ Classes/@UDPBaseSatteliteCommunicator/sendMessage.m
@@ -15,31 +15,24 @@ function transmissionStatus = sendMessage(obj, msgLabel, msgData, varargin)
     udpHandle    = obj.udpHandle;
     
     % Send the leading message label
-    fprintf('Sending message label: %s via udpHandle: %d\n', messageLabel, udpHandle);
     matlabNUDP('send', udpHandle, messageLabel);
     
     % Serialize data
-    fprintf('Serializing data\n');
     byteStream = getByteStreamFromArray(messageData);
      
     % Send number of bytes to read
-    fprintf('Sending# of bytes (%d) for messageData: %s\n', numel(byteStream), messageData);
     matlabNUDP('send', udpHandle, sprintf('%d', numel(byteStream)));
         
     % Send each byte separately
     for k = 1:numel(byteStream)
-       fprintf('Sedning byte %d of %d\n', k, numel(byteStream));
        matlabNUDP('send', udpHandle, sprintf('%03d', byteStream(k)));
     end
     
     % Send the trailing message label
-    fprintf('Sending the trailing message label\n');
     matlabNUDP('send', udpHandle, messageLabel);
        
     % Wait for acknowledgment that the message was received OK
-    fprintf('Waiting for ACK\n');
     timedOutFlag = obj.waitForMessageOrTimeout(timeOutSecs);
-    fprintf('Reading ACK\n');
     if (timedOutFlag)
         executeTimeOut(obj, 'while waiting to receive acknowledgment for message sent', timeOutAction);
         transmissionStatus = obj.NO_ACKNOWLDGMENT_WITHIN_TIMEOUT_PERIOD;
