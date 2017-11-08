d170cf0cc5aaaa88db4b1cc97c69c673879f8797
Nicolas Cottaris <cottaris@sas.upenn.edu>
2017-11-08 13:25:50 -0500

    More sattelite->satellite corrections
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- Classes/@UDPBaseSatelliteCommunicator/instantiateObject.m
+++ Classes/@UDPBaseSatelliteCommunicator/instantiateObject.m
@@ -17,7 +17,7 @@ function UDPobj = instantiateObject(hostNames, hostIPs, hostRoles,  beVerbose)
     commPorts = {nan, 2007, 2008, 2009, 2010, 2011};
         
     % Establish the localIP
-    localHostName = UDPBaseSatteliteCommunicator.getLocalHostName();
+    localHostName = UDPBaseSatelliteCommunicator.getLocalHostName();
     localIP = hostIPs{find(strcmp(lower(hostNames), localHostName))};
     
     % Assemble baseInfo
@@ -25,21 +25,21 @@ function UDPobj = instantiateObject(hostNames, hostIPs, hostRoles,  beVerbose)
     baseInfo.baseHostName = lower(hostNames{baseIndex(1)});
     baseInfo.baseIP = hostIPs{baseIndex(1)};
     
-    % Assemble satteliteInfo
-    satteliteInfo = containers.Map();
-    satteliteIndices = find(strcmp(lower(hostRoles), 'sattelite'));
-    for k = 1:numel(satteliteIndices)
-        d.satteliteChannelID = k-1;
-        d.portNo = commPorts{satteliteIndices(k)};
-        d.satteliteIP = hostIPs{satteliteIndices(k)};
-        satteliteName = lower(hostNames{satteliteIndices(k)});
-        satteliteInfo(satteliteName) = d;
+    % Assemble satelliteInfo
+    satelliteInfo = containers.Map();
+    satelliteIndices = find(strcmp(lower(hostRoles), 'satellite'));
+    for k = 1:numel(satelliteIndices)
+        d.satelliteChannelID = k-1;
+        d.portNo = commPorts{satelliteIndices(k)};
+        d.satelliteIP = hostIPs{satelliteIndices(k)};
+        satelliteName = lower(hostNames{satelliteIndices(k)});
+        satelliteInfo(satelliteName) = d;
     end
         
-    UDPobj = UDPBaseSatteliteCommunicator( ...
+    UDPobj = UDPBaseSatelliteCommunicator( ...
             localIP, ...                       % REQUIRED: the local host IP
             baseInfo, ...                      % REQUIRED: the base info
-            satteliteInfo, ...                 % REQUIRED: the sattelite info
+            satelliteInfo, ...                 % REQUIRED: the satellite info
             'verbosity', verbosity ...         % OPTIONAL, with default value: 'normal', and possible values: {'min', 'normal', 'max'},
     );
 end
