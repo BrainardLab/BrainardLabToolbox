b7c1d2730609055041cf20cbb6155aa85b6663be
melanopsin <melanopsin@gka06.psych.upenn.edu>
2017-11-29 12:05:32 -0500

    debug

--- Classes/@UDPBaseSatelliteCommunicator/waitForMessage.m
+++ Classes/@UDPBaseSatelliteCommunicator/waitForMessage.m
@@ -41,6 +41,8 @@ function packet = waitForMessage(obj, msgLabel, varargin)
     
     % Read the leading packet label
     packet.messageLabel = matlabNUDP('receive', udpHandle);
+    warndlg(sprintf('expected = %s', expectedMessageLabel), sprintf('received = %s', packet.messageLabel));
+    
     if (~strcmp(expectedMessageLabel,'')) && (~strcmp(packet.messageLabel, expectedMessageLabel))
         error('\nLeading message label (''%s'') does not match expected message label (''%s'')\n', packet.messageLabel, expectedMessageLabel);
     end
