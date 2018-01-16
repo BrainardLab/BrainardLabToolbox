0c831771e54b416cc098c05fb7df11dd38302843
Nicolas Cottaris <cottaris@sas.upenn.edu>
2018-01-16 14:04:54 -0500

    Updates
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- Classes/@UDPBaseSatelliteCommunicator/flushQueue.m
+++ Classes/@UDPBaseSatelliteCommunicator/flushQueue.m
@@ -1,11 +1,15 @@
 % Method that flushes the UDP channel when something goes bad during a
 % transmission.
 function flushedContents = flushQueue(obj)
-    % Wait to make sure the sender has sent all the data, then flush it
-    %pause(obj.flushDelay);
+    
+    if (obj.flushDelay > 0)
+        % Wait to make sure the sender has sent all the data
+        pause(obj.flushDelay);
+    end
+    
+    % flush it
     flushedContents = '';
     while (matlabNUDP('check', obj.udpHandle) == 1)
         flushedContents = matlabNUDP('receive', obj.udpHandle);
     end
 end
-
