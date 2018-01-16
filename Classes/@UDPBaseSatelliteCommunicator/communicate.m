956c981054c9a877a44e93974906ff11bcac0070
Nicolas Cottaris <cottaris@sas.upenn.edu>
2018-01-16 14:31:29 -0500

    updates
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- Classes/@UDPBaseSatelliteCommunicator/communicate.m
+++ Classes/@UDPBaseSatelliteCommunicator/communicate.m
@@ -71,7 +71,14 @@ function [messageReceived, status, roundTripDelayMilliSecs, attemptNo] = communi
                             'timeOutSecs', communicationPacket.timeOutSecs ...
                     );
                 otherwise
-                    error('Unknown status: ''%s''.', status);
+                    obj.displayMessage(sprintf('received unexpected status ''%s'' from remote host', status), communicationPacket.messageLabel, communicationPacket.messageData, packetNo, 'alert', true);
+                    attemptNo = attemptNo + 1;
+                    % Pause for 0.5 seconds before resending
+                    pause(0.5)
+                    fprintf('\n<strong>Attempting to send the  message ''%s'' again (attempt #%d)</strong>\n', communicationPacket.messageLabel, attemptNo);
+                    status = obj.sendMessage(communicationPacket.messageLabel, communicationPacket.messageData, ...
+                            'timeOutSecs', communicationPacket.timeOutSecs ...
+                    );
             end % switch
         end % while
 
