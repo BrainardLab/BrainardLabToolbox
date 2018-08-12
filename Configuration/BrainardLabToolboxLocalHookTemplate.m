627271f070f7ba20403efc1fb0eb426d07c7670e
David Brainard <brainard@psych.upenn.edu>
2018-08-12 09:05:09 -0400

    Fix up local hook template so it will still run even if we don’t put all subfolders on the path.
    The local hook uses some path and info functions in BLTB.  Certain prefs not set if these functions are missing.  I think this should be OK because the only times we want those prefs will be times we are using the whole BLTB.

--- Configuration/BrainardLabToolboxLocalHookTemplate.m
+++ Configuration/BrainardLabToolboxLocalHookTemplate.m
@@ -7,6 +7,8 @@
 % 10/15/17  dhb       Only try to stick PTB java stuff on path if we can find the PTB.
 % 10/9/17   npc       Added ptbBaseDir field and case for Nicolas' iMac
 % 11/11/17  dhb       Add 'CalDataFolder' preference.
+% 08/12/18  dhb       Sometimes we only add a subdir of BLTB.  Make this
+%                     work when we don't have GetComputerInfo.
 
 %% Define project
 toolboxName = 'BrainardLabToolbox';
@@ -24,52 +26,59 @@ if (ispref('ColorMaterialModel'))
 end
 
 %% Specify project location
-ptbBaseDir = tbLocateToolbox('BrainardLabToolbox');
+bltbBaseDir = tbLocateToolbox('BrainardLabToolbox');
 
 % Figure out where baseDir for other kinds of data files is.
-sysInfo = GetComputerInfo();
-switch (sysInfo.localHostName)
-    case 'eagleray'
-        % DHB's desktop
-        baseDir = fullfile(filesep,'Volumes','Users1','Dropbox (Aguirre-Brainard Lab)');
- 
-    case {'Manta', 'Manta-2'}
-        % Nicolas's iMac
-        baseDir = fullfile(filesep,'Volumes','DropBoxDisk/Dropbox','Dropbox (Aguirre-Brainard Lab)');
-        
-    otherwise
-        % Some unspecified machine, try user specific customization
-        switch(sysInfo.userShortName)
-            % Could put user specific things in, but at the moment generic
-            % is good enough.
-            otherwise
-                baseDir = ['/Users/' sysInfo.userShortName '/Dropbox (Aguirre-Brainard Lab)'];
-        end
+%
+% Can only do this when we have GetComputerInfo available.
+if (exist('GetComputerInfo','file'))
+    sysInfo = GetComputerInfo();
+    switch (sysInfo.localHostName)
+        case 'eagleray'
+            % DHB's desktop
+            baseDir = fullfile(filesep,'Volumes','Users1','Dropbox (Aguirre-Brainard Lab)');
+            
+        case {'Manta', 'Manta-2'}
+            % Nicolas's iMac
+            baseDir = fullfile(filesep,'Volumes','DropBoxDisk/Dropbox','Dropbox (Aguirre-Brainard Lab)');
+            
+        otherwise
+            % Some unspecified machine, try user specific customization
+            switch(sysInfo.userShortName)
+                % Could put user specific things in, but at the moment generic
+                % is good enough.
+                otherwise
+                    baseDir = ['/Users/' sysInfo.userShortName '/Dropbox (Aguirre-Brainard Lab)'];
+            end
+    end
+    
+    % RadiometerChecks preferences
+    %
+    % These preferences have to do with the RadiometerChecks section of the
+    % BrainardLabToolbox
+    setpref(toolboxName,'RadiometerChecksDir',fullfile(baseDir,'MELA_admin','RadiometerChecks'));
 end
 
 %% ColorMaterialModel preferences
 %
 % These preferences have to do with the ColorMaterialModel section of the
 % BrainardLabToolbox
-setpref(toolboxName,'cmmCodeDir',fullfile(ptbBaseDir,'ColorMaterialModel'));
-setpref(toolboxName,'cmmDemoDataDir', fullfile(ptbBaseDir,'ColorMaterialModel','DemoData'));
+setpref(toolboxName,'cmmCodeDir',fullfile(bltbBaseDir,'ColorMaterialModel'));
+setpref(toolboxName,'cmmDemoDataDir', fullfile(bltbBaseDir,'ColorMaterialModel','DemoData'));
 
-%% RadiometerChecks preferences
+%% Add PTB PsychJava to the path, if we can find the Psychtoolbox.
 %
-% These preferences have to do with the RadiometerChecks section of the
-% BrainardLabToolbox
-setpref(toolboxName,'RadiometerChecksDir',fullfile(baseDir,'MELA_admin','RadiometerChecks'));
-
-% Add PTB PsychJava to the path, if we can find the Psychtoolbox.
-%% Find the toolbox location, the TbTb way.
+% Find the toolbox location, the TbTb way.
 ptbBaseDir = tbLocateToolbox('Psychtoolbox-3');
 if (~isempty(ptbBaseDir))
     thePath = fullfile(ptbBaseDir,'Psychtoolbox','PsychJava');
     theMsg = 'Psychtoolbox/PsychJava';
-    JavaAddToPath(thePath,theMsg);
+    if exist('JavaAddToPath','file')
+        JavaAddToPath(thePath,theMsg);
+    end
 end
 
-% RadiometerChecks need access to GetWithDefault, which is a PTB function
+%% RadiometerChecks need access to GetWithDefault, which is a PTB function
 setpref(toolboxName, 'ptbBaseDir', ptbBaseDir);
 
 %% Calibration prefs
