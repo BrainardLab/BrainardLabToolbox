e957f4f4a47bb8274e5fbadfe9374bc60f98a58e
David Brainard <brainard@psych.upenn.edu>
2019-04-22 09:58:16 -0400

    Add ellipse fitting.  Kind of working.

--- Metropsis/CCTplot.m
+++ Metropsis/CCTplot.m
@@ -35,15 +35,18 @@ function theFig = CCTplot(fName,varargin)
     % Need to point to a data file available on the machine this is being
     % run on.
     CCTplot('/Users/geoffreyaguirre/Documents/Deena CCT spreadsheets/TOME_DEENA_2.txt')
+    CCTplot('/Volumes/Users1/Dropbox (Aguirre-Brainard Lab)/MTRP_data/Exp_CCTE/Subject_TOME_BURGE/TOME_BURGE_1.txt')
+    CCTplot('/Volumes/Users1/Dropbox (Aguirre-Brainard Lab)/MTRP_data/Exp_CCTE/Subject_DEENA ELUL/DEENA ELUL_2 copy.txt')
 %}
+
+% Close figures
+close all;
 
 % Parse key/value pairs
 p = inputParser;
 p.addParameter('figHandle',[],@(x) (isempty(x) | ishandle(x)));
 p.addParameter('plotColor','r',@(x) (ischar(x) | isvector(x)));
-
 p.parse(varargin{:});
-
 
 % Read the file
 [center_v_prime_w, center_u_prime_w, azimuthsTable] = ParseCCTTextfile(fName);
@@ -63,6 +66,25 @@ for i = 1:highestIndex
     v_prime(i) = sin(azimuthsTable(i,3)) * azimuthsTable(i,1) + center_v_prime_w;
 end
 
+% Try to fit an ellipse to the data
+%
+% We have several ellipse fitting routines.
+theData = [u_prime-center_u_prime_w v_prime-center_v_prime_w zeros(size(u_prime))]';
+initialFactor = 15;
+ellRanges = max(theData,[],2)-min(theData,[],2);
+ellParams0 = [1./ellRanges'.^0.5 0 0 0]';
+ellParams0(1:3) = ellParams0(1:3)*initialFactor;
+ellParams0(isinf(ellParams0)) = 1;
+[fitA,fitAinv,fitQ,fitEllParams] = EllipsoidFit(theData,ellParams0,false,true);
+fitCenter = [center_u_prime_w center_v_prime_w 0]';
+
+% Get ellipse from fit
+nThetaEllipse = 200;
+circleIn2D = UnitCircleGenerate(nThetaEllipse);
+circleIn3DPlane = [circleIn2D(1,:) ; circleIn2D(2,:) ; zeros(size(circleIn2D(1,:)))];
+fitEllipseIn3DPlane = PointsOnEllipsoidFind(fitQ,circleIn3DPlane,fitCenter);
+fitEllipseIn2D = fitEllipseIn3DPlane(1:3,:);
+
 % Plot u_prime and v_prime
 if (isempty(p.Results.figHandle))
     theFig = figure; clf; hold on
@@ -70,7 +92,8 @@ if (isempty(p.Results.figHandle))
 end
 set(gca,'FontName','Helvetica','FontSize',16);
 plot(u_prime,v_prime,'ro','MarkerEdgeColor',p.Results.plotColor,'MarkerFaceColor',p.Results.plotColor','MarkerSize',10);
-plot(center_u_prime_w, center_v_prime_w, 'b*','MarkerSize',5) 
+plot(center_u_prime_w, center_v_prime_w, 'b*','MarkerSize',8);
+plot(fitEllipseIn2D(1,:),fitEllipseIn2D(2,:),'g','LineWidth',2);
 xlim([0.1 0.3]);
 ylim([0.35 0.55]);
 axis('square');
