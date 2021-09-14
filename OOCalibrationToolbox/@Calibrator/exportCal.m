7f932ca914ba6d12ec52584feccb651aa77d6020
Nicolas Cottaris <nicolas@n2range-0014.sas.private.upenn.edu>
2021-09-14 16:58:24 -0400

    Export extra params (multiPrimary) to the cal.
    Signed-off-by: Nicolas Cottaris <nicolas@n2range-0014.sas.private.upenn.edu>

--- OOCalibrationToolbox/@Calibrator/exportCal.m
+++ OOCalibrationToolbox/@Calibrator/exportCal.m
@@ -2,6 +2,33 @@ function obj = exportCal(obj)
 
     calStruct = obj.cal;
     
+    % Add extra fields to calStruct
+
+    
+    % Add extra fields to calStruct
+    extraFieldsForCalStruct = {...
+        'customLinearitySetup' ...          
+        'customBackgroundDependenceSetup' ... 
+        'calibratorTypeSpecificParamsStruct' ...
+        'skipLinearityTest' ...
+        'skipBackgroundDependenceTest' ...
+        'skipAmbientLightMeasurement'...
+    };
+    % Get all properties of the obj.options @CalibratorOptions object
+    props = properties(obj.options);
+    % Add the ones that exist
+    extraOptions = [];
+    for fIndex = 1:numel(extraFieldsForCalStruct)
+        theFieldName = extraFieldsForCalStruct{fIndex};
+        if (any(ismember(props, theFieldName)))
+            fprintf('Adding ''%s'' to calStruct\n', theFieldName);
+            extraOptions.(theFieldName) = obj.options.(theFieldName);
+        end
+    end
+    if (~isempty(extraOptions))
+        calStruct.extraOptions = extraOptions;
+    end
+    
     fprintf('Exporting new-style cal format to %s.mat\n', obj.calibrationFile);
     
     BLToolboxPrefs = getpref('BrainardLabToolbox');
