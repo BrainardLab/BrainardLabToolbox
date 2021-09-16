62fb6df2697522318e25c3c8ab4400622301f635
Semin-Oh <ohsemin1990@gmail.com>
2021-09-16 16:11:19 -0400

    Add warming up delay part before each measurement

--- OOCalibrationToolbox/@SACCPrimaryCalibrator/updateStimulusAndMeasure.m
+++ OOCalibrationToolbox/@SACCPrimaryCalibrator/updateStimulusAndMeasure.m
@@ -1,11 +1,7 @@
 % Method to update the stimulus and conduct a single radiometric measurement by 
 % calling the corresponding method of the attached @Radiometer object.
 function [measurement, S] = updateStimulusAndMeasure(obj, bgSettings, targetSettings, useBitsPP)
-%
 
-    % [SEMIN]
-    % Use a loop to modify this to print out values of all settings entries.
-   
     if (obj.options.verbosity > 1)
         for i=1:obj.nSubprimaries
         fprintf('        Target settings %2.0f   : %2.3f \n\n',i,round(252*targetSettings(i)));
@@ -14,7 +10,19 @@ function [measurement, S] = updateStimulusAndMeasure(obj, bgSettings, targetSett
     
     % update background and target stimuli
     obj.updateBackgroundAndTarget(bgSettings, targetSettings, useBitsPP);
-
+    
+    % make a delay before the measurement for warming-up the device
+    timeToDelay = obj.options.calibratorTypeSpecificParamsStruct.LEDWarmupDurationSeconds;
+    fprintf('        Timer will count %2.1f seconds for warming up \n\n',timeToDelay);
+    timerForWarmingup = timer('TimerFcn','stat=false','StartDelay',timeToDelay); % Timer setting
+    start(timerForWarmingup) % Start the timer
+    for t=1:timeToDelay
+         disp('.') % Just displaying the dot to see if the timer is working
+         pause(1) % Pause for 1 second (so, a dot is showing per each every second)
+    end
+    delete(timerForWarmingup); % End the timer
+    disp('        Close the Timer and the measurement will begin');
+    
     % then measure 
     obj.radiometerObj.measure();
     
